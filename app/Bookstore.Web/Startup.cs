aa92e9694e713dc93a3734b2d9a4f0c3b7f2ecfa
NoReply Amazon <no-reply@amazon.com>
2025-02-14 07:16:27 +0000

    Revert "[Commit] Iteration 22 #errors = 7 <= 7."
    This reverts commit 7202c0a6559d7e1c6052667a7433d553a03f8b6d.

--- app/Bookstore.Web/Startup.cs
+++ app/Bookstore.Web/Startup.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Bookstore.Web
 {
@@ -9,58 +8,19 @@ namespace Bookstore.Web
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddLogging(builder =>
-            {
-                builder.AddConfiguration(builder.Services.BuildServiceProvider().GetRequiredService<Microsoft.Extensions.Configuration.IConfiguration>().GetSection("Logging"));
-                builder.AddConsole();
-                builder.AddDebug();
-            });
-
-            // Configure other services here
+            // Configure services here
         }
 
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
-            var loggerFactory = app.ApplicationServices.GetRequiredService<ILoggerFactory>();
-            var logger = loggerFactory.CreateLogger<Startup>();
-
-            logger.LogInformation("Configuring application...");
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-
-            // Keeping the original method calls, but logging their execution
-            logger.LogInformation("Configuring logging...");
             LoggingSetup.ConfigureLogging();
 
-            logger.LogInformation("Configuring configuration...");
             ConfigurationSetup.ConfigureConfiguration();
 
-            logger.LogInformation("Configuring dependency injection...");
+            // Update these methods to work with ASP.NET Core
             DependencyInjectionSetup.ConfigureDependencyInjection(app);
 
-            logger.LogInformation("Configuring authentication...");
             AuthenticationConfig.ConfigureAuthentication(app);
-
-            logger.LogInformation("Application configured successfully.");
         }
     }
 }
