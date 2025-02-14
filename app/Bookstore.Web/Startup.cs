64a74e7fb7be6e71a70264db98f2f4ba25ad8ccd
NoReply Amazon <no-reply@amazon.com>
2025-02-14 07:27:12 +0000

    [Commit] Iteration 26 #errors = 7 <= 7.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Startup.cs][CS0103][18,13] The name 'ConfigurationSetup' does not exist in the current context``.

--- app/Bookstore.Web/Startup.cs
+++ app/Bookstore.Web/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Bookstore.Common; // Assuming ConfigurationSetup is in this namespace
 
 namespace Bookstore.Web
 {
