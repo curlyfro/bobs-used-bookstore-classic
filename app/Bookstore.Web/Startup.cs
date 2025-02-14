540c254ac6958f9e1ca6acc2e9fbe7a64b1c95da
NoReply Amazon <no-reply@amazon.com>
2025-02-14 07:29:54 +0000

    [Commit] Iteration 27 #errors = 5 <= 7.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Startup.cs][CS0103][18,13] The name 'ConfigurationSetup' does not exist in the current context``.

--- app/Bookstore.Web/Startup.cs
+++ app/Bookstore.Web/Startup.cs
@@ -1,26 +1,55 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Bookstore.Web
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             // Configure services here
+            services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
-            LoggingSetup.ConfigureLogging();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
 
-            ConfigurationSetup.ConfigureConfiguration();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            // Use the _configuration object instead of ConfigurationSetup
+            // ConfigurationSetup.ConfigureConfiguration();
 
             // Update these methods to work with ASP.NET Core
             DependencyInjectionSetup.ConfigureDependencyInjection(app);
 
             AuthenticationConfig.ConfigureAuthentication(app);
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
